7975ae54e129bd5029ddf6403c5ad0575a827525
Admin <pratiyushverma6@gmail.com>
2023-08-23 18:27:55 +0530

    frontend 1.0

---
+++ app/src/main/java/com/example/claculator_app20/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.claculator_app20;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
